1d59aba73135bb2106deb195e9894c2a1dd43af9
hshaosf <11635576+hshaosf@users.noreply.github.com>
2020-04-08 21:56:36 -0700

    login

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
+require('dotenv').config()
+
 const express = require("express");
+
 const app = express();
 
 const port = 5000;
+
+const basicAuth = require('express-basic-auth')
+
+app.use("/", basicAuth({
+  challenge: true,
+  realm: 'test-data',
+  users: { "user" : process.env.LOGIN_PASS },
+  unauthorizedResponse: 'Restricted area.'
+}));
 
 // Body parser
 app.use(express.urlencoded({ extended: false }));
@@ -9,12 +21,16 @@ app.use(express.urlencoded({ extended: false }));
 // Listen on port 5000
 app.listen(port, () => {
   console.log(`Server is booming on port 5000
-Visit http://localhost:5000`);
+http://localhost:5000`);
 });
 
 app.get("/", (req, res) => {
-    res.send("Welcome to a basic express App");
+    res.send("Welcome to a basic express App "+process.env.LOGIN_USER);
   });
+
+app.post("/", (req, res) => {
+  res.send("Welcome");
+});
   
   // Mock APIs
   app.get("/users", (req, res) => {
